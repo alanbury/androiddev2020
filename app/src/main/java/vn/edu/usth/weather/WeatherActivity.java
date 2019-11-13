d4e5021420a3d2ac237046d2871d1eacea75c4e2
Alan Bury <alanbury98@gmail.com>
2019-11-13 14:00:04 +0700

    practical work 3 (crashed)

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -10,6 +10,8 @@ private static final String TAG = "WeatherActivity";
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
+        ForecastFragment firstFragment = new ForecastFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
     }
 
     @Override
